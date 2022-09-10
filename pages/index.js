b13b3150a72aab8ef392d989daa5835b0225b1c5
MitySoftDOM <mity.math369@gmail.com>
2022-09-10 09:15:02 -0400

    links fixed

--- pages/index.js
+++ pages/index.js
@@ -27,19 +27,18 @@ export default function Home() {
         <title>RE&RAM</title>
         <meta content="" name="description" />
         <meta content="" name="keywords" />
-        {/* Favicons */}
-        <link href="img/favicon.png" rel="icon" />
-        <link href="img/apple-touch-icon.png" rel="apple-touch-icon" />
-        {/* Vendor CSS Files */}
-        <link href={"vendor/aos/aos.css"} rel="stylesheet" />
-        <link href={"vendor/bootstrap/css/bootstrap.min.css"} rel="stylesheet" />
-        <link href={"vendor/bootstrap-icons/bootstrap-icons.css"} rel="stylesheet" />
-        <link href={"vendor/boxicons/css/boxicons.min.css"} rel="stylesheet" />
-        <link href={"vendor/glightbox/css/glightbox.min.css"} rel="stylesheet" />
-        <link href={"vendor/remixicon/remixicon.css"} rel="stylesheet" />
-        <link href={"vendor/swiper/swiper-bundle.min.css"} rel="stylesheet" />
-        {/* Template Main CSS File */}
-        <link href={"css/style.css"} rel="stylesheet" />
+
+        {/* <!-- Vendor CSS Files --> */}
+        <link href="vendor/aos/aos.css" rel="stylesheet" />
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+        <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+        <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+        <link href="vendor/remixicon/remixicon.css" rel="stylesheet" />
+        <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+        {/* <!-- Template Main CSS File --> */}
+        <link href="css/style.css" rel="stylesheet" />
       </Head>
       {/* Header section */}
       <Header />
@@ -648,6 +647,7 @@ export default function Home() {
         </div>
       </Fragment>
 
+
       {/* <!-- Vendor JS Files --> */}
       <script src={"vendor/purecounter/purecounter_vanilla.js"} async />
       <script src={"vendor/aos/aos.js"} async />
@@ -657,6 +657,7 @@ export default function Home() {
       <script src={"vendor/swiper/swiper-bundle.min.js"} async />
       {/* <!-- Template Main JS File --> */}
       <script src={"js/main.js"} async />
+
     </>
   );
 } 
